f2fb3cc2fc99cb78e34b2e5286824249afe745a2
ztoufik <toufikouedtlelat@gmail.com>
2025-08-12 20:10:13 +0100

    test setup with gtest

--- build.ps1
+++ build.ps1
@@ -1,6 +1,3 @@
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja -S . -B build
-cd build
-ninja
-cd ..\bin\
-.\generic_executable.exe
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON  -DCMAKE_BUILD_TYPE=Debug -G Ninja -S . -B build;
+cd build && ninja && ctest;
 cd ..
